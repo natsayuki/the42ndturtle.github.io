9979acc342dbb404724f9b3ebda6e287082410ca
madeline <the42ndturtle@gmail.com>
2022-02-10 01:54:54 -0500

    added ls command to term and tab shortcut to open new term

--- components/comp-term.js
+++ components/comp-term.js
@@ -2,12 +2,40 @@ Vue.component('term', {
   data: function(){
     return {
       text: '',
+      printed: []
     }
   },
   methods: {
     handleCommand(){
+      this.printCommand();
+      if(this.$data.text == 'ls'){
+        this.print('there');
+        this.print('will');
+        this.print('be');
+        this.print('projects');
+        this.print('and');
+        this.print('shit');
+        this.print('here');
+      }
 
+      else{
+        this.print(`unrecognized command ${this.$data.text.split(' ')[0]}`);
+      }
+
+      this.$data.text = '';
     },
+    print(text){
+      const w = this.$el.querySelector('.term-printed-wrapper');
+      w.innerHTML += `
+      <div class="term-text">${text}</div>
+      `
+    },
+    printCommand(){
+      const w = this.$el.querySelector('.term-printed-wrapper');
+      w.innerHTML += `
+      <span class="term-username">madeline@madeline</span><span class="term-text">:~$ ${this.$data.text}</span>
+      `
+    }
   },
   mounted() {
     document.addEventListener('keydown', e => {
@@ -25,6 +53,9 @@ Vue.component('term', {
   template: `
     <div class="term">
       <div class="term-wrapper">
+        <div class="term-printed-wrapper">
+
+        </div>
         <div><span class="term-username">madeline@madeline</span><span class="term-text">:~$ {{text}}</span><span class="term-cursor">▮</span></div>
       </div>
     </div>
--- script.js
+++ script.js
@@ -6,11 +6,20 @@ const projects = {
 const data = {
   message: "ur nan",
   projects: projects,
+  numTerms: 0,
 }
 
 const methods = {
 
 }
+
+function createTerminal(){
+  data.numTerms++;
+}
+
+document.addEventListener('keydown', e => {
+  if(e.key == "Tab") createTerminal();
+});
 
 const vm = new Vue({
   el: '#app',
