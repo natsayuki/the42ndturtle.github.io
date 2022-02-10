00d5647465554cfebda10904906dee3621a5367a
madeline <the42ndturtle@gmail.com>
2022-02-10 04:41:51 -0500

    added cd and cat commands as well as dir structure

--- components/comp-term.js
+++ components/comp-term.js
@@ -2,24 +2,56 @@ Vue.component('term', {
   data: function(){
     return {
       text: '',
-      printed: []
+      printed: [],
+      '~': {
+        projects: {
+          type: 'dir'
+        },
+        'about-me.txt': {
+          type: 'file',
+          content: "Hello! My name is Madeline! I am currently attending college at NC State university for Computer Science. I like programming and video games, though I am bad at both!"
+        },
+        experience: {
+          type: 'dir',
+          'unity.txt': {
+            type: 'file',
+            content: 'unity internship'
+          }
+        }
+      },
+      path: ['~'],
     }
   },
   methods: {
     handleCommand(){
-      this.printCommand();
-      if(this.$data.text == 'ls'){
-        this.print('there');
-        this.print('will');
-        this.print('be');
-        this.print('projects');
-        this.print('and');
-        this.print('shit');
-        this.print('here');
-      }
+      const command = this.$data.text.split(' ');
 
+      this.printCommand();
+
+      if(command[0] == 'ls'){
+        const root = eval(`this.$data['${this.$data.path.join("']['")}']`);
+        Object.keys(root).forEach(item => {
+          if(root[item].type == 'dir') this.printDir(item)
+          if(root[item].type == 'file') this.print(item);
+        });
+      }
+      else if(command[0] == 'cd'){
+        const root = eval(`this.$data['${this.$data.path.join("']['")}']`);
+        // if(command[1] == '../' && this.$data.path != ['~']){
+        //   this.$data.path = this.$data.path[0,this.$data.path.length-1]
+        // }
+        Object.keys(root).forEach(item => {
+          if(root[item].type == 'dir' && item == command[1]) this.$data.path.push(item)
+        });
+      }
+      else if(command[0] == 'cat'){
+        const root = eval(`this.$data['${this.$data.path.join("']['")}']`);
+        Object.keys(root).forEach(item => {
+          if(root[item].type == 'file' && item == command[1]) this.print(root[item].content);
+        });
+      }
       else{
-        this.print(`unrecognized command ${this.$data.text.split(' ')[0]}`);
+        this.print(`unrecognized command ${command[0]}`);
       }
 
       this.$data.text = '';
@@ -30,10 +62,16 @@ Vue.component('term', {
       <div class="term-text">${text}</div>
       `
     },
+    printDir(text){
+      const w = this.$el.querySelector('.term-printed-wrapper');
+      w.innerHTML += `
+      <div class="term-text-dir">${text}</div>
+      `
+    },
     printCommand(){
       const w = this.$el.querySelector('.term-printed-wrapper');
       w.innerHTML += `
-      <span class="term-username">madeline@madeline</span><span class="term-text">:~$ ${this.$data.text}</span>
+      <span class="term-username">madeline@madeline</span><span class="term-text">:${this.$data.path.join('/')}$ ${this.$data.text}</span>
       `
     }
   },
@@ -56,7 +94,7 @@ Vue.component('term', {
         <div class="term-printed-wrapper">
 
         </div>
-        <div><span class="term-username">madeline@madeline</span><span class="term-text">:~$ {{text}}</span><span class="term-cursor">▮</span></div>
+        <div><span class="term-username">madeline@madeline</span><span class="term-text">:{{path.join('/')}}$ {{text}}</span><span class="term-cursor">▮</span></div>
       </div>
     </div>
   `
